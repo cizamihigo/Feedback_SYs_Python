3b3b2cb05f072c7e34039f07fd0e1d9d273b5113
cizamihigo <cizamihigochristian@gmail.com>
2021-02-07 23:08:13 +0200

    Login Implementation

--- index.py
+++ index.py
@@ -9,14 +9,14 @@ THANKS for Being part with us.\n\
 Pord@TeAm\n********\n===============================================\n\n")
 
 
-try:
-    choix = input("What do you want? Login(1) or signUp???(2)\t: ")
-    if int(choix) == 1 :
-        print("\t\t\t\t-------------\n\t\t\t\t| Login Form |\n\t\t\t\t-------------")
-        logins.login()
-    elif len(choix) > 1:
-        print("Try again Later. 1 or 2 not other types of caracters\n")
-except ValueError() :
-    print("Your choice is either a text instead of 1 or 2. Try later!!!")
-except TypeError():
-    print("Your choice is either a text instead of 1 or 2. Try later!!!")
+
+
+choix = input("What do you want? Login (1) or signUp??? (2)\t: ")
+if int(choix) == 1 :
+    print("\t\t\t\t-------------\n\t\t\t\t| Login Form |\n\t\t\t\t-------------")
+    logins.login()
+elif len(choix) > 1:
+    print("Try again Later. 1 or 2 not other types of caracters\n")
+elif int(choix) == 2:
+    print("\t\t\t\t---------------\n\t\t\t\t| Sign Up Form |\n\t\t\t\t---------------")
+    logins.add_user('S')
--- logins.py
+++ logins.py
@@ -4,18 +4,18 @@ con = sqlite3.connect("Logins.db")
 coCur = con.cursor()
 
 # coCur.execute("CREATE TABLE Log(Id INTEGER PRIMARY KEY AUTOINCREMENT, UserName VARCHAR(50) NOT NULL, Password VARCHAR(255) NOT NULL, UserType VARCHAR(2) DEFAULT 'U')")
-def add_user():
+def add_user(usertype):
     print("ADDING NEW USER FORM")
     print("********************")
     UserName = input("Enter your Username: (@fulani):\t")
     Password = input("Enter User Password:           \t")
     if usertype == 'S':
         Nusertype = input("Enter a user Type: ('U','A','S'):\t")
-        if len(UserName) < = 50 and len(Password) < = 200 and len(Nusertype) < 2 and ( Nusertype='S' or Nusertype = 'A' or Nusertype = 'U'):
+        if len(UserName) <  51 and len(Password) < 201 and len(Nusertype) < 2 and ( Nusertype =='S' or Nusertype == 'A' or Nusertype == 'U'):
             cosel = con.cursor()
             cosel.execute("SELECT UserName FROM Log WHERE Username = ?",[UserName])
             selet = cosel.fetchall()
-            if selet[0] == []:
+            if selet == []:
                 coCur.execute("INSERT INTO Log(UserName, Password, UserType ) VALUES(?,?,?)",(UserName,Password,Nusertype))
                 con.commit()
                 print("\t\t\t\t\tUSer Added Successfully!")
@@ -26,11 +26,12 @@ def add_user():
         elif len(Nusertype) > 2 or (Nusertype != 'S' or Nusertype != 'A' or Nusertype != 'U' ):
             print("User type should only be  S or U or A :\t")
     else:
+        Nusertype = 'U'
         if len(UserName) < 51 and len(Password) < 201 and len(Nusertype) < 2:
             cosel = con.cursor()
             cosel.execute("SELECT UserName FROM Log WHERE Username = ?",[UserName])
             selet = cosel.fetchall()
-            if selet[0] == []:
+            if selet == []:
                 coCur.execute("INSERT INTO Log(UserName, Password) VALUES(?,?)",(UserName,Password))
                 con.commit()
                 print("\t\t\t\t\tUSer Added Successfully!")
@@ -38,11 +39,30 @@ def add_user():
                 print("User Name Already registered in our Database. Please choose another one.")
         elif len(UserName) > 50:
             print("This Username Is to long to remember Choose a simple one")
-    con.close()
-    coCur.close()
-    cosel.close()
+    # con.close()
+    # coCur.close()
+    # cosel.close()
 
 def login():
     Name = input("Enter Your UserName: \t")
-    Password = input("Enter Your Password:\t",NotImplemented)
+    Password = input("Enter Your Password:\t")
+    conn = sqlite3.connect('Logins.db')
+    cue = conn.cursor()
+    cue.execute("SELECT * FROM Log WHERE UserName = ? AND Password = ?",(Name, Password))
+    vcue = cue.fetchall()
+    if vcue == []:
+        print("No such User In our Databse Please Sign Up first")
+        print("Do you Want To Sign Up (Y) or (N) 1 or 0: ")
+        signup = input("\t")
+        if signup == 'Y' or signup == 'y' or signup == int('1'):
+            add_user()
+        elif signup == 'N' or signup == 'n' or signup == int('0'):
+            print("Good Bye!!!!!\n\n")
+            quit()
+        else:
+            print("Not a choice! Good Bye!")
+            quit()
+    else:
+        print(vcue)
+        print("WELCOME USER. Your personal info are such: \nUsername = {0}\nUser Type = {1}".format(vcue[0][1],vcue[0][3] )
         
